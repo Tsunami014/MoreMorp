60f79f23247c9a5ebe773813d72112e1654114fb
Tsunami014 <tsunami014@duck.com>
2026-04-15 07:19:45 +1000

    Made it override fetch requests!
    We're getting places!

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Test!");
+const s = document.createElement("script");
+
+// Otherwise it wouldn't load properly
+s.textContent = `
+  const originalFetch = window.fetch;
+  window.fetch = function (...args) {
+    console.log("Intercepted fetch:", args);
+    return originalFetch.apply(this, args);
+  };
+
+  console.log("MoreMorp successfully loaded!");
+`;
+
+// Inject immediately
+document.documentElement.appendChild(s);
+s.remove();
