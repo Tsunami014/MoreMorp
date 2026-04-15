d1bd7ed799aedd5ec7027b67df5b35d59d132349
Tsunami014 <tsunami014@duck.com>
2026-04-15 16:58:33 +1000

    Added a miniature cidery to the town square :)

--- src/injct.js
+++ src/injct.js
@@ -1,6 +1,16 @@
 function modifyJSON(url, js) {
     //console.log(url, js);
-    js.npcs = []
+    if (url.includes("town-square")) {
+        js.objects.push({
+            id: "9999999999999_aaaaaaa",
+            // action?
+            rotation: 0.4,
+            scale: 1.2,
+            type: "1771541102415_smgqcvx", // cidery
+            x: -8,
+            z: -2,
+        })
+    }
 }
 
 function hook() {}
