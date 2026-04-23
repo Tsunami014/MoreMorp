88dfaab22658d0aa5a99f3fb9052d5749567ef09
Tsunami014 <tsunami014@duck.com>
2026-04-23 12:40:55 +1000

    Improved the world a bit
    Added some rocks

--- src/replace.js
+++ src/replace.js
@@ -5,6 +5,9 @@ const PATCH = {
     "1776899749834_jppll7d.webp",
     "1776899750304_q4qsn5a.webp",
     "1776899750607_1v2yasx.webp",
+    "1776908449364_2k7hklt.webp",
+    "1776908589495_g3q8l7e.webp",
+    "1776908622945_9pp2pso.webp",
   ]},
   LEVELS: {
   prefix: "levels/", data: [
